4d6eda4d55461723587735b4ac7e45e295b74e0b
Veniamin <47269098+veniaminvelkov@users.noreply.github.com>
2023-06-08 13:42:07 +0300

    Created a paint management service.

---
+++ ApplicationService/Implementations/PaintManagementService.cs
@@ -0,0 +1,172 @@
+﻿using ApplicationService.DTOs;
+using Data.Entities;
+using Repository.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationService.Implementations
+{
+    public class PaintManagementService
+    {
+        /*
+    -ID <int>
+	-Paint type: <int>
+		-Default (Unspecified) [0]
+		-Oil paint [1]
+		-Acrylic paint [2]
+		-Watercolor paint [3]
+		-Gouache [4]
+		-Error [-1 or any other number]
+	-Brand <string>
+	-Price <double>
+	-Aditional info <string>
+	-Is available <bool>
+	-Expiry date <DateTime>
+         */
+        public List<PaintDTO> Get()
+        {
+            List<PaintDTO> paintDTO = new List<PaintDTO>();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                foreach (var item in unitOfWork.PaintRepository.Get())
+                {
+                    paintDTO.Add(new PaintDTO
+                    {
+                        Id = item.Id,
+                        Brand = item.Brand,
+                        ExpiryDate = item.ExpiryDate,
+                        IsAvailable = item.IsAvailable,
+                        PaintType = item.PaintType,
+                        Price = item.Price,
+                        Quantity = item.Quantity
+                    });
+                }
+            }
+            return paintDTO;
+        }
+        public List<PaintDTO> GetAvailable()
+        {
+            List<PaintDTO> paintDTO = new List<PaintDTO>();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                foreach (var item in unitOfWork.PaintRepository.Get())
+                {
+                    if (item.IsAvailable == true && item.Quantity > 0)
+                    {
+                        paintDTO.Add(new PaintDTO
+                        {
+                            Id = item.Id,
+                            Brand = item.Brand,
+                            ExpiryDate= item.ExpiryDate,
+                            IsAvailable = true,
+                            PaintType= item.PaintType,
+                            Price= item.Price,
+                            Quantity = item.Quantity
+                        });
+                    }
+                }
+            }
+            return paintDTO;
+        }
+        public PaintDTO GetById(int id)
+        {
+            PaintDTO paintDTO = new PaintDTO();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Paint paint = unitOfWork.PaintRepository.GetByID(id);
+                if (paint != null)
+                {
+                    paintDTO = new PaintDTO
+                    {
+                        Id = paint.Id,
+                        Brand = paint.Brand,
+                        ExpiryDate = paint.ExpiryDate,
+                        IsAvailable = paint.IsAvailable,
+                        PaintType = paint.PaintType,
+                        Price = paint.Price,
+                        Quantity = paint.Quantity
+                    };
+                }
+            }
+            return paintDTO;
+        }
+        public PaintDTO GetOnlyAvailableById(int id)
+        {
+            PaintDTO paintDTO = new PaintDTO();
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Paint paint = unitOfWork.PaintRepository.GetByID(id);
+                if (paint != null && paint.IsAvailable == true)
+                {
+                    paintDTO = new PaintDTO
+                    {
+                        Id = paint.Id,
+                        Brand = paint.Brand,
+                        ExpiryDate = paint.ExpiryDate,
+                        IsAvailable = paint.IsAvailable,
+                        PaintType = paint.PaintType,
+                        Price = paint.Price,
+                        Quantity = paint.Quantity
+                    };
+                }
+            }
+            return paintDTO;
+        }
+        public bool Save(PaintDTO paintDTO)
+        {
+            Paint paint = new Paint
+            {
+                Id = paintDTO.Id,
+                Brand = paintDTO.Brand,
+                ExpiryDate = paintDTO.ExpiryDate,
+                IsAvailable = paintDTO.IsAvailable,
+                PaintType = paintDTO.PaintType,
+                Price = paintDTO.Price,
+                Quantity = paintDTO.Quantity
+            };
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                unitOfWork.PaintRepository.Insert(paint);
+                unitOfWork.Save();
+            }
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                using (UnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    Paint paint = unitOfWork.PaintRepository.GetByID(id);
+                    unitOfWork.EmployeeRepository.Delete(paint);
+                    unitOfWork.Save();
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool MakeUnavailable(int id)
+        {
+
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                Paint paint = unitOfWork.PaintRepository.GetByID(id);
+                paint.IsAvailable = false;
+                unitOfWork.Save();
+            }
+
+            return true;
+        }
+    }
+}
