b0dfb63d6f5eeb4a372efc881afee5ee573d5595
Veniamin <47269098+veniaminvelkov@users.noreply.github.com>
2023-06-08 15:51:57 +0300

    Created an order WCF service.

---
+++ WcfServiceLibrary/IOrder.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationService.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IOrder" in both code and config file together.
+    [ServiceContract]
+    public interface IOrder
+    {
+        [OperationContract]
+        string Message();
+
+        [OperationContract]
+        List<OrderDTO> GetOrders();
+
+        [OperationContract]
+        OrderDTO GetOrderByID(int id);
+
+        [OperationContract]
+        string PostOrder(OrderDTO orderDTO);
+
+        [OperationContract]
+        string DeleteOrder(int id);
+    }
+}
---
+++ WcfServiceLibrary/Order.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationService.DTOs;
+using ApplicationService.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Order" in both code and config file together.
+    public class Order : IOrder
+    {
+        private OrderManagementService service = new OrderManagementService();
+
+        public List<OrderDTO> GetOrders()
+        {
+            return service.Get();
+        }
+
+        public OrderDTO GetOrderByID(int id)
+        {
+            return service.GetById(id);
+        }
+
+        public string PostOrder(OrderDTO orderDTO)
+        {
+            if (!service.Save(orderDTO))
+                return "Order is not inserted";
+
+            return "Order is inserted";
+        }
+
+        public string DeleteOrder(int id)
+        {
+            if (!service.Delete(id))
+                return "Order is not deleted";
+
+            return "Order is deleted";
+        }
+        public string Message()
+        {
+            return "The service is up and running.";
+        }
+    }
+}
